b3baf6234b2a60ccc474105da520a37117d06068
youngheepark <dugy70@gmail.com>
2019-10-23 15:30:58 +0900

    p 변수이름 변경
    변수이름을 좀더 명시적인 이름으로 변경하였음 (p --> product)

--- shopping.cc
+++ shopping.cc
@@ -23,7 +23,7 @@ void Shopping::PrintList() {
 // Sort shopping_list_ Elements by Price
 // Apply Selection Sort algorithm
 void Shopping::SortByPrice() {
-  std::vector<Goods>::iterator p = shopping_list_.begin();
+  std::vector<Goods>::iterator product = shopping_list_.begin();  // review 의견 반영
   std::vector<Goods>::iterator min = shopping_list_.end();
   for (std::vector<Goods>::iterator i = shopping_list_.begin();
       i != shopping_list_.end()-1; i++) {
